589254ed21f461d5a2a012471abbb0c33d6e6f22
pooriabahremand <106535603+pooriabahremand@users.noreply.github.com>
2024-07-06 15:45:42 +0330

    reparing the eslint rules

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    remotePatterns: [toRemotePattern(process.env.CMS_IMAGE_PATTERN)],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+      toRemotePattern(process.env.CMS_IMAGE_PATTERN),
+    ],
   },
 };
 
