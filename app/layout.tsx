f820082ecbc97a89d50f328f4c931d609d6bcf79
pooriabahremand <106535603+pooriabahremand@users.noreply.github.com>
2024-07-07 13:25:31 +0330

    last touches on root layout and adding comments to it

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,38 @@
-import NavBar from "../components/navBar";
-import React from "react";
-import "./globals.css";
-import { orbitron, exo2 } from "./fonts";
+import NavBar from "../components/navBar"; // Import the NavBar component
+import React from "react"; // Import React
+import "./globals.css"; // Import global CSS styles
+import { orbitron, exo2 } from "./fonts"; // Import custom fonts
 
+// Define metadata for the document
 export const metadata = {
   title: {
-    default: "Indie Gamer",
-    template: "%s | Indie Gamer",
+    default: "Indie Gamer", // Default title
+    template: "%s | Indie Gamer", // Title template for dynamic titles
   },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+// Define the props interface with a readonly children prop of type React.ReactNode
+interface Props {
+  children: Readonly<React.ReactNode>;
+}
+
+// Define the RootLayout component, which wraps the app's layout
+export default function RootLayout({ children }: Props) {
   return (
-    <html lang="en" className={`${orbitron.variable} ${exo2.variable}`}>
-      <body className="bg-orange-50 flex flex-col px-4 py-2 min-h-screen">
+    <html lang="en" className={`${orbitron.variable} ${exo2.variable}`}> {/* Set language and apply custom fonts */}
+      <body className="bg-orange-50 flex flex-col px-4 py-2 min-h-screen"> {/* Set background color, flex layout, padding, and minimum height */}
         <header>
-          <NavBar />
+          <NavBar /> {/* Render the navigation bar */}
         </header>
-        <main className="py-3 grow">{children}</main>
-        <footer className="border-t py-3 text-center text-xs text-slate-500">
+        <main className="py-3 grow">{children}</main> {/* Render the children prop within the main content area, with padding and flex-grow */}
+        <footer className="border-t py-3 text-center text-xs text-slate-500"> {/* Render the footer with a top border, padding, centered text, and small text size */}
           Game data and images courtesy of{" "}
           <a
             href="https://rawg.io/"
             target="_blank"
             className="text-orange-800 hover:underline"
           >
-            RAWG
+            RAWG {/* Link to the RAWG website */}
           </a>
         </footer>
       </body>
